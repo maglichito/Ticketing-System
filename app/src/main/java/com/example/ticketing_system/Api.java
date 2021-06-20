9e66534e2381a94304a41fc18ef61cf0deabe11f
Almir Palamar <palamar995@gmail.com>
2021-06-20 16:47:48 +0200

    api-login

---
+++ app/src/main/java/com/example/ticketing_system/Api.java
@@ -0,0 +1,12 @@
+package com.example.ticketing_system;
+
+public class Api {
+    public String login = "ticketingsystemfscg.000webhostapp.com/ticketing_api/login.php"; //login api
+
+
+    public String getLogin() {
+        return login;
+    }
+}
+
+
