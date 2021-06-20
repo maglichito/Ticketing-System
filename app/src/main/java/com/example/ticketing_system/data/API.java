92c71599d627770f554752a4e9b94e18de345fb7
Almir Palamar <palamar995@gmail.com>
2021-06-20 19:54:43 +0200

    signup-api

--- app/src/main/java/com/example/ticketing_system/data/API.java
+++ app/src/main/java/com/example/ticketing_system/data/API.java
@@ -2,7 +2,7 @@ package com.example.ticketing_system.data;
 
 public class API {
     public String login = "https://ticketingsystemfscg.000webhostapp.com/ticketing_api/login.php"; //login api
-
+    public String signup = "https://ticketingsystemfscg.000webhostapp.com/ticketing_api/signup.php"; //signup api
 
     public String getLogin() {
         return login;
