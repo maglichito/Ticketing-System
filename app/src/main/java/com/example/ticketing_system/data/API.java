f89f34832914c5eb9b26f15b0f45eb55f86b7032
Almir Palamar <palamar995@gmail.com>
2021-06-21 00:14:49 +0200

    pass_change API

--- app/src/main/java/com/example/ticketing_system/data/API.java
+++ app/src/main/java/com/example/ticketing_system/data/API.java
@@ -3,12 +3,11 @@ package com.example.ticketing_system.data;
 public class API {
     public String login = "https://ticketingsystemfscg.000webhostapp.com/ticketing_api/login.php"; //login api
     public String signup = "https://ticketingsystemfscg.000webhostapp.com/ticketing_api/signup.php"; //signup api
+    public String pass_change = "https:///ticketingsystemfscg.000webhostapp.com/ticketing_api/profile.php"; //change password
 
-    public String getLogin() {
-        return login;
-    }
-
+    public String getLogin() { return login; }
     public String getSignup() { return signup; }
+    public String getPass_change() {return pass_change; }
 }
 
 
