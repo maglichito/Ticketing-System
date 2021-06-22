6c6c654ba4f5d22bc0f6f0cc73da0f84f1f967b2
Almir Palamar <palamar995@gmail.com>
2021-06-23 00:02:39 +0200

    edit-profile-api

--- app/src/main/java/com/example/ticketing_system/data/API.java
+++ app/src/main/java/com/example/ticketing_system/data/API.java
@@ -3,11 +3,13 @@ package com.example.ticketing_system.data;
 public class API {
     public String login = "https://ticketingsystemfscg.000webhostapp.com/ticketing_api/login.php"; //login api
     public String signup = "https://ticketingsystemfscg.000webhostapp.com/ticketing_api/signup.php"; //signup api
-    public String pass_change = "https:///ticketingsystemfscg.000webhostapp.com/ticketing_api/profile.php"; //change password
+    public String pass_change = "https:///ticketingsystemfscg.000webhostapp.com/ticketing_api/change_password.php"; //change password
+    public String prof_change = "https:///ticketingsystemfscg.000webhostapp.com/ticketing_api/change_profile.php"; //change profile
 
     public String getLogin() { return login; }
     public String getSignup() { return signup; }
     public String getPass_change() {return pass_change; }
+    public String getProf_change() {return prof_change; }
 }
 
 
